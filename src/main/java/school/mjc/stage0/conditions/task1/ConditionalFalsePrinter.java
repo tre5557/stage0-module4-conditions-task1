5cf8725fb105803a3de11c92c17394696f42bc04
Timur Bashirov <bashirov188@gmail.com>
2024-03-26 20:44:56 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if(2<3) {
+            System.out.println(true);
+        }
+            else {
+                System.out.println(false);
+            }
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        if(2>3){
+            System.out.println(true);
+        }
     }
 }
